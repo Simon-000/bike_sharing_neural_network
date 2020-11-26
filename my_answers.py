2dec7e9275c16392c7ad13a2c1748fb9253380b1
Simon-000 <Simon-000>
2020-11-26 18:53:31 +0000

    Added [:, None] slicing to both delta_weights_* objects

--- my_answers.py
+++ my_answers.py
@@ -99,9 +99,9 @@ class NeuralNetwork(object):
         hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
         
         # Weight step (input to hidden)
-        delta_weights_i_h += hidden_error_term * X
+        delta_weights_i_h += hidden_error_term * X[:, None]
         # Weight step (hidden to output)
-        delta_weights_h_o += output_error_term * hidden_outputs
+        delta_weights_h_o += output_error_term * hidden_outputs[:,None]
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
